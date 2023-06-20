0c3b15907b4529b1db4ad460daf59e54f82d120b
Noel <noel@noel.com>
2023-06-20 17:55:43 -0600

    routing

---
+++ src/pages/guitarras/[url].js
@@ -0,0 +1,28 @@
+
+
+import { useRouter } from "next/router"
+
+export default function Producto({guitarra}) {
+
+console.log(guitarra[0].attributes.nombre);
+
+  return (
+    <div>[url]</div>
+  )
+}
+
+export async function getServerSideProps({query: {url}}) {
+  console.log(url);
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`)
+  const {data: guitarra } = await respuesta.json()
+
+ 
+
+  return{
+
+    props: {
+      guitarra
+    }
+
+  }
+}
